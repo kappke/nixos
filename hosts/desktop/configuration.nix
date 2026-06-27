b7e17980714cf4458911a54315e1c4f44c41dc95
Vinícius Kappke <vinikappke@gmail.com>
2026-06-27 10:52:58 -0300

    change desktop setup

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,23 +1,93 @@
-{ config, pkgs, ... }:
+# /etc/nixos/configuration.nix
 
+{ config, inputs, pkgs, ... }:
+
+let
+  wallpaper = pkgs.copyPathToStore ./wallpapers/wallhaven-6llkol.png;
+
+  gtkgreetCSS = pkgs.writeText "gtkgreet.css" ''
+    window {
+      background-color: transparent;
+    }
+  '';
+
+  swayConfig = pkgs.writeText "greetd-sway-config" ''
+    output * bg ${wallpaper} fill
+
+    exec "${pkgs.gtkgreet}/bin/gtkgreet -l -s ${gtkgreetCSS}; swaymsg exit"
+
+    bindsym Mod4+shift+e exec swaynag \
+      -t warning \
+      -m 'What do you want to do?' \
+      -b 'Poweroff' 'systemctl poweroff' \
+      -b 'Reboot' 'systemctl reboot'
+  '';
+in
 {
-  imports = [ ./hardware-configuration.nix ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" "uinput" ];
-
-  nixpkgs.config.allowUnfree = true;
-
-  # --- Remote Management & Flakes ---
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    trusted-users = [ 
-      "root"
-      "kappke"
-      "@wheel"
-    ];
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+      };
+      initial_session = {
+        command = "sway";
+        user = "kappke";
+      };
+    };
   };
+
+  environment.etc."greetd/environments".text = ''
+    sway
+    bash
+  '';
+
+
+  imports =
+    [ 
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    plymouth = {
+      enable = true;
+      theme = "rings";  # or "bgrt" for OEM logo
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "rings" ];
+        })
+      ];
+    };
+
+    # Silent boot configuration
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
+    kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" "uinput" ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "kappke-desktop"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "systemd-resolved";
+  networking.firewall.trustedInterfaces = [ "docker0" ];
+  networking.firewall.allowedTCPPorts = [ 22 8080 ];
 
   services.openssh = {
     enable = true;
@@ -27,12 +97,15 @@
     };
   };
 
-  networking.hostName = "gaming-node";
-  networking.networkmanager.enable = true;
-  networking.interfaces.enp5s0.wakeOnLan.enable = true; 
+  services.resolved = {
+    enable = true;
+    fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
+  };
 
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
   hardware.nvidia = {
     modesetting.enable = true;
@@ -41,44 +114,24 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  swapDevices = [ { device = "/var/lib/swapfile"; size = 8192; } ];
-  
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true; 
-  };
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
-
-  programs.gamemode.enable = true;
-
-  programs.zsh.enable = true;
-  users.users.kappke = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "video" "render" ];
-  };
-
-  # Automatic login is required for Sunshine to capture the desktop
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "kappke";
-  };
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Keyring for password management
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
+  programs.seahorse.enable = true;
+  services.flatpak.enable = true;
+  xdg.portal.enable = true;
 
+  services.tailscale = {
+    enable = true;
+  };
+
+  # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
@@ -91,23 +144,108 @@
     LC_TIME = "pt_BR.UTF-8";
     LC_CTYPE = "pt_BR.UTF-8";
   };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "ibus";
+    ibus.engines = with pkgs.ibus-engines; [];
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "br";
+    variant = "";
+  };
+
+  # Configure console keymap
   console.keyMap = "br-abnt2";
 
-  # fileSystems."/mnt/storage" = {
-  #   device = "/dev/disk/by-uuid/4A32A1DB32A1CBEF";
-  #   fsType = "ntfs";
-  # };
+  security.polkit.enable = true;
+  programs.zsh.enable = true;
 
-  # 9. System Packages
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
+  programs.gamemode.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kappke = {
+    isNormalUser = true;
+    description = "Vinícius Kappke";
+    extraGroups = [ "networkmanager" "wheel" "video" "render" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [];
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "kappke" = import ../../users/kappke.nix;
+    };
+  };
+
+  virtualisation.docker = {
+    enable = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
+    # programming
+    go
+    bun
+    nodejs
+    gcc
+    cmake
+    gnumake
+
+    # utilities
+    wget
+    zip
+    unzip
+    tree
+
+    # apps
+    btop # process monitor
+    oxker # docker TUI
+    kitty # terminal emulator
+    ghostty # terminal emulator
+    github-cli
+
+    # gaming
     heroic
     mangohud
-    git
-    vim
   ];
 
-  networking.firewall.allowedTCPPorts = [ 22 27036 27037 47984 47989 48010 ];
-  networking.firewall.allowedUDPPorts = [ 47998 47999 48000 48002 48010 27031 27036 ];
+  fonts.packages = with pkgs; [
+    # ui
+    roboto
+    roboto-slab
+    roboto-mono
+    roboto-serif
+    fira-code
+    fira-code-symbols
+    # nerdfonts
+  ];
 
-  system.stateVersion = "25.11";
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 32 * 1024;
+    }
+  ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 }
